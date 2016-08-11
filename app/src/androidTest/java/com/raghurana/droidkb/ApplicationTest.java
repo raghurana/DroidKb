62e0e9316bcf6941b2a50435bf0d8af0bb224ed1
Raghu Rana <raghurana@gmail.com>
2016-08-11 21:50:39 +1000

    Initial commit.

---
+++ app/src/androidTest/java/com/raghurana/droidkb/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.raghurana.droidkb;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest
+        extends ApplicationTestCase<Application> {
+
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+
+}
