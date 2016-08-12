e6580efc842a46c10c460a0b57248b851e3d9bde
Raghu Rana <raghurana@gmail.com>
2016-08-12 11:55:11 +1000

    Added click data binding to the button on MainActivity.

--- DroidKbModule/src/main/java/com/raghurana/droidkb/MainActivity.java
+++ DroidKbModule/src/main/java/com/raghurana/droidkb/MainActivity.java
@@ -1,14 +1,29 @@
 package com.raghurana.droidkb;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.databinding.tool.Binding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.raghurana.droidkb.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setPresenter(this);
+    }
+
+    public void onMaterialDesignSurfaceClick() {
+        Intent intent = new Intent(this, MaterialSurface.class);
+        startActivity(intent);
     }
 
 }
