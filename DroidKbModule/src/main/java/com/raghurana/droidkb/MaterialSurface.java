97e7d939e6a245fc3fd73c838964b4c7b9a1739a
Raghu Rana <raghurana@gmail.com>
2016-08-12 10:49:38 +1000

    material surface class added.

---
+++ DroidKbModule/src/main/java/com/raghurana/droidkb/MaterialSurface.java
@@ -0,0 +1,13 @@
+package com.raghurana.droidkb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MaterialSurface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_surface);
+    }
+}
